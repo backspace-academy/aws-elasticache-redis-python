5334ed6a4d08b4f62417515e38e17222a68e4a2a
BackSpace Academy <31625808+backspace-academy@users.noreply.github.com>
2020-07-27 06:20:03 +1000

    Update elasticache.py

--- elasticache.py
+++ elasticache.py
@@ -1,1 +1,22 @@
+# Import the Redis package
+import redis
 
+redisEndpoint = 'YOUR_REDIS_ENDPOINT'
+# Get the port and host from the endpoint string
+PORT = redisEndpoint[-4:];
+HOST = redisEndpoint[:-5];
+
+# Configure Redis connection
+def config_redis_connection():
+    r = redis.Redis(
+        host=HOST,
+        port=PORT)
+    print('Configured Redis node connection: ')
+    print(r)
+    return r
+
+# Main program
+def main():
+    redis_connection = config_redis_connection()
+if __name__ == '__main__':
+    main()
